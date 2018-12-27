57f9acd9198bd84dd8f61b48e7df971b0ba87b79
Edsion_Chen <annie520930@163.com>
2018-12-27 16:55:35 +0800

    商品列表

--- src/main/java/com/blog/dusk/DuskApplication.java
+++ src/main/java/com/blog/dusk/DuskApplication.java
@@ -4,11 +4,19 @@ import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.blog.dusk.mapper")
-public class DuskApplication {
+public class DuskApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DuskApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DuskApplication.class, args);
---
+++ src/main/resources/static/js/CommonJs.js
@@ -0,0 +1,12 @@
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+document.write('<link rel=\"stylesheet\" href=\"http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css\">');
+
+<!-- 可选的Bootstrap主题文件（一般不用引入） -->
+document.write('<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">');
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+document.write('<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>');
+document.write('<script src="https://cdn.bootcss.com/jquery/1.12.1/jquery.js"></script>');
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+document.write('<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>');
